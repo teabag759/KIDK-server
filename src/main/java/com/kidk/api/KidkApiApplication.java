f5b72aa11543f13419ab8de949cfb812ad171df4
Soy(Lee SoHyeon) <127467306+teabag759@users.noreply.github.com>
2025-12-23 09:38:21 +0900

    #32/feat: 시간대 설정

--- src/main/java/com/kidk/api/KidkApiApplication.java
+++ src/main/java/com/kidk/api/KidkApiApplication.java
@@ -2,12 +2,19 @@ package com.kidk.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class KidkApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KidkApiApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
